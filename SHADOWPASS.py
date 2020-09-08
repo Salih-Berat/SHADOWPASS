b5d5a6475d3091700f345616767344e78de578ef
Hacke333 <yagizegeurtac@gmail.com>
2020-09-08 21:31:03 +0300

    Fixed rce and added some features

--- SHADOWPASS.py
+++ SHADOWPASS.py
@@ -1,62 +1,67 @@
+#!/usr/bin/env python3
+#-*-encoding:utf8-*-
 print("""BU PROGRAM KALİTRİK TARAFINDAN YAPILMIŞTIR!
 BÜTÜN HAKLARI KALİTRİK'E AİTTİR!\n
 THİS PROGRAM WRİTTED BY KALITRIK!""")
 print("""SHADOWPASS UYGULAMASI\n
 ŞİFRELEME METODU 1-2 GÜNDE BİR DEĞİŞMEKTEDİR\n
 LÜTFEN GİTHUB'UMU TAKİP EDİNİZ!\n""")
-i="o"
-o="i"
-ı="ü"
-ü="ı"
-e="o"
-o="e"
-a="ö"
-ö="a"
-b="z"
-z="b"
-c="y"
-y="c"
-ç="v"
-v="ç"
-d="t"
-t="d"
-f="ş"
-ş="f"
-g="s"
-s="g"
-ğ="n"
-n="ğ"
-h="m"
-m="h"
-j="l"
-l="j"
-p="r"
-r="p"
-e="u"
-u="e"
-o="i"
-i="o"
-k="k"
-w="x"
-x="w"
+enc={
+"i":"o",
+"o":"i",
+"ı":"ü",
+"ü":"ı",
+"e":"o",
+"o":"e",
+"a":"ö",
+"ö":"a",
+"b":"z",
+"z":"b",
+"c":"y",
+"y":"c",
+"ç":"v",
+"v":"ç",
+"d":"t",
+"t":"d",
+"f":"ş",
+"ş":"f",
+"g":"s",
+"s":"g",
+"ğ":"n",
+"n":"ğ",
+"h":"m",
+"m":"h",
+"j":"l",
+"l":"j",
+"p":"r",
+"r":"p",
+"e":"u",
+"u":"e",
+"o":"i",
+"i":"o",
+"k":"k",
+"w":"x",
+"x":"w",
+" ":" "
+}
 print("SHADOWPASS V1.0\n")
 print("ŞİFRELER VE ÇÖZER\n")
 print("""Programı kullanmak için
  örnek olarak
- print(ş,i,f,r,e,l,e,n,c,e,k,k,e,l,i,m,e)
- veya 
- print(ş+i+f+r+e+l+e+n+e+c+e+k+k+e+l+i+m+e) şeklinde yazınız.
+ şifrelencek kelime
  Programda Şifrelenmiş Kelimeyi Çözmek içinse
  örnek olarak:
- print(ç,ö,z,ü,l,e,c,e,k,k,e,l,i,m,e)
- veya
- print(ç+ö+z+ü+l+e+c+e+k+k+e+l+i+m+e)
- NOT:BOŞLUK BUGU,DÖNGÜ MODU VE print(...) KELİMESİ YAZMA YERİ YAKINDA DÜZELTİLECEKTİR""")
-q = input("Şifrele veya Çöz=")
-cevap = exec(q)
-print(cevap)
-input()
-
-
-
-
+ çözülecek kelime""")
+while 1:
+	q = input("Şifrele veya Çöz:")
+	out=""
+	oldlen=len(q)
+	q=list(q)
+	while len(out)!=oldlen:
+		for a,b in enc.items():
+			if q[0]==a:
+				out+=b
+				del q[0]
+				break
+	q=''.join(q)
+	print(out)
