8ae39670788fe1aac0dd7f1674cbf723e79af1bb
KALITRIK <70946597+KALITRIK@users.noreply.github.com>
2020-09-08 15:07:44 +0300

    Add files via upload

---
+++ SHADOWPASS.py
@@ -0,0 +1,62 @@
+print("""BU PROGRAM KALİTRİK TARAFINDAN YAPILMIŞTIR!
+BÜTÜN HAKLARI KALİTRİK'E AİTTİR!\n
+THİS PROGRAM WRİTTED BY KALITRIK!""")
+print("""SHADOWPASS UYGULAMASI\n
+ŞİFRELEME METODU 1-2 GÜNDE BİR DEĞİŞMEKTEDİR\n
+LÜTFEN GİTHUB'UMU TAKİP EDİNİZ!\n""")
+i="o"
+o="i"
+ı="ü"
+ü="ı"
+e="o"
+o="e"
+a="ö"
+ö="a"
+b="z"
+z="b"
+c="y"
+y="c"
+ç="v"
+v="ç"
+d="t"
+t="d"
+f="ş"
+ş="f"
+g="s"
+s="g"
+ğ="n"
+n="ğ"
+h="m"
+m="h"
+j="l"
+l="j"
+p="r"
+r="p"
+e="u"
+u="e"
+o="i"
+i="o"
+k="k"
+w="x"
+x="w"
+print("SHADOWPASS V1.0\n")
+print("ŞİFRELER VE ÇÖZER\n")
+print("""Programı kullanmak için
+ örnek olarak
+ print(ş,i,f,r,e,l,e,n,c,e,k,k,e,l,i,m,e)
+ veya 
+ print(ş+i+f+r+e+l+e+n+e+c+e+k+k+e+l+i+m+e) şeklinde yazınız.
+ Programda Şifrelenmiş Kelimeyi Çözmek içinse
+ örnek olarak:
+ print(ç,ö,z,ü,l,e,c,e,k,k,e,l,i,m,e)
+ veya
+ print(ç+ö+z+ü+l+e+c+e+k+k+e+l+i+m+e)
+ NOT:BOŞLUK BUGU,DÖNGÜ MODU VE print(...) KELİMESİ YAZMA YERİ YAKINDA DÜZELTİLECEKTİR""")
+q = input("Şifrele veya Çöz=")
+cevap = eval(q)
+print(cevap)
+input()
+
+
+
+
